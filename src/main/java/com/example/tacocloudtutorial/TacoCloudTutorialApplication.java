178b306c1467dd4c6bc2ac8a85f5e40eb1d2f99e
yarkovok <yarkokvok@wp.pl>
2022-06-28 19:02:30 +0200

    first

---
+++ src/main/java/com/example/tacocloudtutorial/TacoCloudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudTutorialApplication.class, args);
+	}
+
+}
